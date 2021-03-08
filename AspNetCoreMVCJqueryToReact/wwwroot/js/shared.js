a2aad6980e6a86813f2fd498368b4696d2f8e3c3
egnomerator <egnomerator@users.noreply.github.com>
2021-03-08 01:58:03 -0600

    a small introduction of react into the app without breaking anything - loading react and react dom from cdn - rendering a raw string via react as part of the footer

--- AspNetCoreMVCJqueryToReact/wwwroot/js/shared.js
+++ AspNetCoreMVCJqueryToReact/wwwroot/js/shared.js
@@ -26,3 +26,24 @@ function clearFilters() {
 function reloadTable() {
     crewTable.ajax.reload();
 }
+
+function renderFooter() {
+    var html =
+    '<footer class="border-top footer text-muted">'+
+        '<div class="container">'+
+            //'&copy; 2021 - AspNetCoreMVCJqueryToReact'+
+            '&copy;<span class="container"></span>'
+        '</div>'+
+    '</footer>'
+    $('#footer-container').html(html);
+    ReactDOM.render(
+        React.createElement(FooterMsgPart),
+        document.querySelector("#footer-container div.container span.container")
+    )
+}
+
+class FooterMsgPart extends React.Component {
+    render() {
+        return '2021 - AspNetCoreMVCJqueryToReact'
+    }
+}
