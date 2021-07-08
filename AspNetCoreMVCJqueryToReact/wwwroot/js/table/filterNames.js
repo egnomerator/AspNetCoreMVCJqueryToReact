2153874f362aa1c5ed27745d95a32999e341a14d
egnomerator <egnomerator@users.noreply.github.com>
2021-07-08 00:16:21 -0500

    A PROBLEM, react component now maintains internal state, everything appears to work from user perspective BUT react does not update its internal state when "clear filters" is used

--- AspNetCoreMVCJqueryToReact/wwwroot/js/table/filterNames.js
+++ AspNetCoreMVCJqueryToReact/wwwroot/js/table/filterNames.js
@@ -5,19 +5,24 @@
 function renderNamesDropdown(names) {
     var selectOptions = {
         o: [
-            { value: '', label: 'Select a Name', selected: 'selected="selected"' },
-            { value: names.charlie, label: names.charlie, selected: '' },
-            { value: names.mac, label: names.mac, selected: '' },
-            { value: names.dennis, label: names.dennis, selected: '' },
-            { value: names.dee, label: names.dee, selected: '' },
-            { value: names.frank, label: names.frank, selected: '' }
+            { value: '', label: 'Select a Name' },
+            { value: names.charlie, label: names.charlie },
+            { value: names.mac, label: names.mac },
+            { value: names.dennis, label: names.dennis },
+            { value: names.dee, label: names.dee },
+            { value: names.frank, label: names.frank }
         ]
     }
 
     ReactDOM.render(
         React.createElement(
             SelectSimple,
-            { colFilterName: "Name", cssClasses: "crew-column-filter custom-select custom-select-sm", options: selectOptions }
+            {
+                colFilterName: "Name",
+                cssClasses: "crew-column-filter custom-select custom-select-sm",
+                options: selectOptions,
+                selectedOption: ""
+            }
         ),
         document.getElementById("filter-names")
     )
