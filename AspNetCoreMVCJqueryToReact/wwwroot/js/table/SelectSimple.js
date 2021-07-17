8a56705871ac0bfb43427734d804b487e37ad004
egnomerator <egnomerator@users.noreply.github.com>
2021-07-17 16:55:17 -0500

    cleaned up the react select component so it now only maintains what it needs to in its state property

--- AspNetCoreMVCJqueryToReact/wwwroot/js/table/SelectSimple.js
+++ AspNetCoreMVCJqueryToReact/wwwroot/js/table/SelectSimple.js
@@ -5,15 +5,10 @@
         this.handleChangeSelected = this.handleChangeSelected.bind(this);
         this.handlePubSubClearFilters = this.handlePubSubClearFilters.bind(this);
         this.state = {
-            colFilterName: props.colFilterName,
-            cssClasses: props.cssClasses + ' ml-1',
-            options: props.options,
-            selectedOption: props.selectedOption,
-            pubSubSubscriber: props.pubSubSubscriber,
-            pubSubEvent: props.pubSubEvent
+            selectedOption: props.selectedOption
         };
 
-        this.state.pubSubSubscriber(this.state.pubSubEvent, this.handlePubSubClearFilters);
+        this.props.pubSubSubscriber(this.props.pubSubEvent, this.handlePubSubClearFilters);
     }
 
     handlePubSubClearFilters() {
@@ -26,11 +21,11 @@
 
     getSelectReact() {
         var selectOptions = [];
-        this.state.options.o.map((option, i) =>
+        this.props.options.o.map((option, i) =>
             selectOptions.push(
-                React.createElement("option", { value: option.value, key: 'col-filter-' + this.state.colFilterName + '-' + i }, option.label)));
+                React.createElement("option", { value: option.value, key: 'col-filter-' + this.props.colFilterName + '-' + i }, option.label)));
 
-        return React.createElement("select", { className: this.state.cssClasses, value: this.state.selectedOption, onChange: this.handleChangeSelected }, selectOptions);
+        return React.createElement("select", { className: this.props.cssClasses + ' ml-1', value: this.state.selectedOption, onChange: this.handleChangeSelected }, selectOptions);
     }
 
     render() {
