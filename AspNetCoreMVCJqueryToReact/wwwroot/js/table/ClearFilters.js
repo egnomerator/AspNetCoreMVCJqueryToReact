298b224af9adce4dd9adcef4f873edce8ab5b928
egnomerator <egnomerator@users.noreply.github.com>
2021-08-01 15:01:58 -0500

    the clear filters button is now a React component--React simply renders the button and does nothing else with it

---
+++ AspNetCoreMVCJqueryToReact/wwwroot/js/table/ClearFilters.js
@@ -0,0 +1,16 @@
+﻿class ClearFilters extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    getClearFiltersReact() {
+        return React.createElement(
+            "a",
+            { className: this.props.cssClasses },
+            this.props.label);
+    }
+
+    render() {
+        return this.getClearFiltersReact();
+    }
+}
