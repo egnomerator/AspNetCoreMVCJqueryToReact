cf4b2aab60d2e43d23a0dd37082f561f87a926ff
egnomerator <egnomerator@users.noreply.github.com>
2021-08-22 22:56:40 -0500

    - removed append version tag helper from script tag referencing the webpack output bundle file in the _Layout - updated webpack to conditionally use source map only if building for development - updated syntax in npm config scripts to set mode switch properly - updated syntax in webpack config to use "fullhash" instead of deprecated "hash" - updated gitignore to ignore the components dist folder

--- AspNetCoreMVCJqueryToReact/webpack.config.js
+++ AspNetCoreMVCJqueryToReact/webpack.config.js
@@ -1,38 +1,44 @@
 ﻿const path = require("path");
 var appPath = __dirname;
 
-module.exports = {
-    context: appPath,
-    devtool: "source-map",
-    entry: {
-        bundle: [
-            "./wwwroot/components/API.js"
-        ]
-    },
-    output: {
-        path: path.resolve(appPath, "wwwroot/components/dist"),
-        filename: "[name].js?[hash]",
-        publicPath: "~/wwwroot/components/dist/",
-        library: {
-            name: "ComponentApi",
-            type: "var"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            ["@babel/preset-env"],
-                            ["@babel/preset-react"]
-                        ]
+module.exports = (env, argv) => {
+    var config = {
+        context: appPath,
+        entry: {
+            bundle: [
+                "./wwwroot/components/API.js"
+            ]
+        },
+        output: {
+            clean: true,
+            path: path.resolve(appPath, "wwwroot/components/dist"),
+            filename: "[name].js?[fullhash]",
+            publicPath: "~/wwwroot/components/dist/",
+            library: {
+                name: "ComponentApi",
+                type: "var"
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                ["@babel/preset-env"],
+                                ["@babel/preset-react"]
+                            ]
+                        }
                     }
                 }
-            }
-        ]
-    }
+            ]
+        }
+    };
+
+    if (argv.mode === "development") { config.devtool = "eval-source-map"; }
+
+    return config;
 };
