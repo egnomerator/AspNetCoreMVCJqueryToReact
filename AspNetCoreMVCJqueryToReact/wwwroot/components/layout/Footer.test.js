24944e38dcae8cf98af8bc881df6787131f0e7ae
egnomerator <egnomerator@users.noreply.github.com>
2021-08-29 21:40:49 -0500

    added support for unit testing with jest - installed exact versions of supporting npm packages as dev dependencies - updated package json to use jest in the existing test script - updated package json with additional script to run jest in watch mode - when jest builds for testing it uses babel to transpile es6 modules - extracted babel presets from webpack config into babel config file to be used by both webpack and jest builds - added first test

---
+++ AspNetCoreMVCJqueryToReact/wwwroot/components/layout/Footer.test.js
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import ReactTestRenderer from "react-test-renderer";
+import Footer from "../layout/Footer";
+
+describe("Footer", () => {
+    it("should render as footer string value", () => {
+        const expectedFooterValue = "2021 - AspNetCoreMVCJqueryToReact";
+
+        const footer = ReactTestRenderer.create(React.createElement(Footer));
+        const footerValue = footer.toJSON();
+
+        expect(footerValue).toBe(expectedFooterValue);
+    });
+});
