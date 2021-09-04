fe48312d65b40258f1f1a051cb6fc1c269eb7b5a
egnomerator <egnomerator@users.noreply.github.com>
2021-09-04 14:42:57 -0500

    the non-module, plain Javascript files now get intellisense on the module bundle API exposed by webpack - for background--existing information about this app:     - after bundling the modules, webpack is configured to expose the bundle in a global variable so the other non-module, plain Javascript files can operate on the API at the root of the compiled bundle     - this is already working, but to develop against this API from within the non-module, plain Javascript files you have to know the API or look at the module files to see the API     - these committed changes, enable the use of intellisense to develop against this API from within the non-module, plain Javascript files

---
+++ AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/api.d.ts
@@ -0,0 +1,5 @@
+﻿declare namespace ClientApp.Components {
+    function renderFooter(container: HTMLElement): void;
+    function renderSelect(container: HTMLElement, props: object): void;
+    function renderClearFiltersButton(container: HTMLElement, props: object): void
+}
