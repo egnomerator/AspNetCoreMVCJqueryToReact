46cdc08b42236d89d271b5ac79d4bd2ad30f54f8
egnomerator <egnomerator@users.noreply.github.com>
2021-09-06 21:47:56 -0500

    updated unit test covering footer component to expect according to the recent change to the footer component--footer is now a span tag containing a string rather than just a string

--- AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer.test.js
+++ AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer.test.js
@@ -3,12 +3,15 @@ import ReactTestRenderer from "react-test-renderer";
 import Footer from "./Footer";
 
 describe("Footer", () => {
-    it("should render as footer string value", () => {
-        const expectedFooterValue = "2021 - AspNetCoreMVCJqueryToReact";
+    it("should render", () => {
+        const expectedTag = "span";
+        const expectedText = "2021 - AspNetCoreMVCJqueryToReact";
 
         const footer = ReactTestRenderer.create(React.createElement(Footer));
         const footerValue = footer.toJSON();
 
-        expect(footerValue).toBe(expectedFooterValue);
+        expect(footerValue.type).toBe(expectedTag);
+        expect(footerValue.children.length).toBe(1);
+        expect(footerValue.children[0]).toBe(expectedText);
     });
 });
