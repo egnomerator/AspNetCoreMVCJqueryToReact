6b019cdfff7f38ad345c61d881083d44920dbb6e
egnomerator <egnomerator@users.noreply.github.com>
2021-11-28 15:58:45 -0600

    updated unit test to be a snapshot test

--- AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.test.tsx
+++ AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.test.tsx
@@ -4,14 +4,8 @@ import Footer from ".";
 
 describe("Footer", () => {
     it("should render", () => {
-        const expectedTag = "span";
-        const expectedText = "2021 - AspNetCoreMVCJqueryToReact";
-
         const footer = ReactTestRenderer.create(<Footer/>);
-        const footerValue = footer.toJSON() as ReactTestRenderer.ReactTestRendererJSON;
 
-        expect(footerValue.type).toBe(expectedTag);
-        expect(footerValue.children.length).toBe(1);
-        expect(footerValue.children[0]).toBe(expectedText);
+        expect(footer).toMatchSnapshot();
     });
 });
