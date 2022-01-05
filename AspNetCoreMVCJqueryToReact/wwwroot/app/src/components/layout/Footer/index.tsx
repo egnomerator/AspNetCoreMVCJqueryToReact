b60ecd337bad676cd6cd11bfba77ba0208626ee1
egnomerator <egnomerator@users.noreply.github.com>
2022-01-05 00:10:07 -0600

    cleaned up code handling footer message--moved handling of copyright symbol into react component; there are all kinds of sources giving ways to handle character encoding in react components--there are warnings of problems from "double-escaping" due to react's automatic escaping for security reasons; i could not duplicate issues, all the ways i tried worked without a problem--keeping a few of these ways for reference

--- AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.tsx
+++ AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.tsx
@@ -1,7 +1,15 @@
 ﻿import * as React from "react";
 
 export default class FooterMsgPart extends React.Component {
+
+    getValue() {
+        return <div>&copy; 2021 - AspNetCoreMVCJqueryToReact</div>;
+    }
+
     render() {
-        return <span>2021 - AspNetCoreMVCJqueryToReact</span>;
+        return this.getValue();
+        //return <div>&copy; 2021 - AspNetCoreMVCJqueryToReact</div>;
+        //return <div>&#169; 2021 - AspNetCoreMVCJqueryToReact</div>;
+        //return <span>{String.fromCharCode(169) + " 2021 - AspNetCoreMVCJqueryToReact"}</span>;
     }
 }
--- AspNetCoreMVCJqueryToReact/wwwroot/js/footer.js
+++ AspNetCoreMVCJqueryToReact/wwwroot/js/footer.js
@@ -1,14 +1,6 @@
 ﻿registeredInitializations.footer.push(renderFooter);
 
 function renderFooter() {
-    var html =
-        '<footer class="border-top footer text-muted">' +
-        '<div class="container">' +
-        //'&copy; 2021 - AspNetCoreMVCJqueryToReact'+
-        '&copy;<span class="container"></span>'
-    '</div>' +
-        '</footer>'
-    $('#footer-container').html(html);
-    var container = document.querySelector("#footer-container div.container span.container");
+    var container = document.querySelector("#footer-container div.container");
     ClientApp.Components.renderFooter(container);
 }
