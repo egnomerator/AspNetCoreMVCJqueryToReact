faf1940c994881a6713dcae65fbbbcbacf79288a
egnomerator <egnomerator@users.noreply.github.com>
2022-01-05 00:12:23 -0600

    cleaned up footer react component

--- AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.tsx
+++ AspNetCoreMVCJqueryToReact/wwwroot/app/src/components/layout/Footer/index.tsx
@@ -1,15 +1,7 @@
 ﻿import * as React from "react";
 
 export default class FooterMsgPart extends React.Component {
-
-    getValue() {
-        return <div>&copy; 2021 - AspNetCoreMVCJqueryToReact</div>;
-    }
-
     render() {
-        return this.getValue();
-        //return <div>&copy; 2021 - AspNetCoreMVCJqueryToReact</div>;
-        //return <div>&#169; 2021 - AspNetCoreMVCJqueryToReact</div>;
-        //return <span>{String.fromCharCode(169) + " 2021 - AspNetCoreMVCJqueryToReact"}</span>;
+        return <span>&copy; 2021 - AspNetCoreMVCJqueryToReact</span>;
     }
 }
